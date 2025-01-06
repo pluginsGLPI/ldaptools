6fec644a03d095a3622e5c8ce9b3f17dc399b60a
Stanislas <skita@teclib.com>
2025-01-06 12:13:27 +0100

    0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+## [0.1.0] - 2025-01-06
+
 ### Feat
 
-- Add support for LDAP replications 
+- Add support for LDAP replications
--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_LDAPTOOLS_VERSION', '0.0.3');
+define('PLUGIN_LDAPTOOLS_VERSION', '0.1.0');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_LDAPTOOLS_MIN_GLPI', '9.5.0');
